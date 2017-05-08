b1c3b56ae2ff7901b4a118e00524feb88799d508
danakock <danakock@me.com>
2017-05-07 23:40:07 -0700

    Run Length Encoding

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,12 @@
-def decode():
-    pass
+from re import sub
+
+def decode(word):
+    return sub(r'(\d+)(\D)', lambda m: m.group(2) * int(m.group(1)),word)
 
 
-def encode():
-    pass
+def encode(word):
+    return sub(r'(.)\1+', lambda m: str(len(m.group(0))) + m.group(1),word)
+
+
+
+encode('AABBBCCCC')
